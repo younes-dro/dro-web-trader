111b674f869c557805cb99aba8144c6187e39284
younes-dro <dro66_8@hotmail.fr>
2019-01-23 01:27:40 +0100

    resoleve all errors. version 1.0.4

--- header.php
+++ header.php
@@ -93,7 +93,9 @@
                     $dro_web_trader_description = get_bloginfo('description', 'display');
                     if ($dro_web_trader_description || is_customize_preview()) :
                         ?>
-                        <p class="site-description"><?php esc_html_e( $dro_web_trader_description,'dro-web-trader' );  ?></p>
+                        <p class="site-description">
+                            <?php echo $dro_web_trader_description;  ?>
+                        </p>
                     <?php endif; ?>
                 </div><!-- .title-box -->
         </div><!-- .site-branding -->
